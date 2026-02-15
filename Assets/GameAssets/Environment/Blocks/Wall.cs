4cdb5a7d8e50cc9a6f7fd545ae4a1be0e6078cdd
Nicole <nicholaslangley@shaw.ca>
2026-02-14 19:39:50 -0700

    made walls have a collider instead of just numerical properties, added some manual walls to the gameplay scene for testing

--- Assets/GameAssets/Environment/Blocks/Wall.cs
+++ Assets/GameAssets/Environment/Blocks/Wall.cs
@@ -6,22 +6,8 @@ using UnityEngine;
 
 public class Wall : Placeable
 {
-    //how tall the wall is from each orientation
-    float heightFromBottom, heightFromTop, heightFromLeft, heightFromRight;
-    
-    //how thick the wall is (for landing on)
-    private float wallThickness;
-
     [SerializeField]
     public bool blocksMovement;
-
-    public void SetHeights(float bot, float top, float left, float right)
-    {
-        heightFromBottom = bot;
-        heightFromTop = top;
-		heightFromLeft = left;
-		heightFromRight = right;
-    }
 
     // Start is called before the first frame update
     public void BlockMovement()
